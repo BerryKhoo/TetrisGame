e16fced214ab3781670283cf6da411d792faea23
Berry <berrykhoo919@gmail.com>
2025-02-27 22:45:39 +0800

    Rotation

--- TetrisGame/TetrisBlock.cpp
+++ TetrisGame/TetrisBlock.cpp
@@ -9,6 +9,7 @@ Colors blockcolor = getColors();
 //Creating tetris Blocks
 TetrisBlock::TetrisBlock(){
 	cellSize = 30;
+	Rotation currentRotation(Rotation::UP);
 }
 
 //Turning it by changing location, dont static hardcode the block type.
@@ -17,49 +18,70 @@ TetrisBlock::TetrisBlock(){
 TetrisBlock LBlock() {
 	TetrisBlock Lblock;
 	Lblock.blockID = 1;
-	Lblock.cells[0] = {BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2)};
+	Lblock.matrix = {0, 0, 1,
+					 1, 1, 1,
+					 0, 0, 0};
+	Lblock.dimension = 3;
 	return Lblock;
 }
 
 TetrisBlock JBlock() {
 	TetrisBlock Jblock;
 	Jblock.blockID = 2;
-	Jblock.cells[0] = { BlockPosition(0,0),BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2) };
+	Jblock.matrix = {1, 0, 0,
+					 1, 1, 1,
+					 0, 0, 0};
+	Jblock.dimension = 3;
 	return Jblock;
 }
 
 TetrisBlock OBlock() {
 	TetrisBlock Oblock;
-	Oblock.blockID = 2;
-	Oblock.cells[0] = { BlockPosition(0,1),BlockPosition(0,2),BlockPosition(1,1),BlockPosition(1,2) };
+	Oblock.blockID = 3;
+	Oblock.matrix = {1, 1,
+					 1, 1};
+	Oblock.dimension = 2;
 	return Oblock;
 }
 
 TetrisBlock SBlock() {
 	TetrisBlock Sblock;
-	Sblock.blockID = 1;
-	Sblock.cells[0] = { BlockPosition(0,1),BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1) };
+	Sblock.blockID = 4;
+	Sblock.matrix = {0, 1, 1,
+					 1, 1, 0,
+					 0, 0, 0};
+	Sblock.dimension = 3;
 	return Sblock;
 }
 
 TetrisBlock ZBlock() {
 	TetrisBlock Zblock;
-	Zblock.blockID = 1;
-	Zblock.cells[0] = { BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1),BlockPosition(1,2) };
+	Zblock.blockID = 5;
+	Zblock.matrix = {1, 1, 0,
+					 0, 1, 1,
+					 0, 0, 0};
+	Zblock.dimension = 3;
 	return Zblock;
 }
 
 TetrisBlock TBlock() {
 	TetrisBlock Tblock;
-	Tblock.blockID = 1;
-	Tblock.cells[0] = { BlockPosition(0,1),BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2) };
+	Tblock.blockID = 6;
+	Tblock.matrix = {0, 1, 0,
+					 1, 1, 1,
+					 0, 0, 0};
+	Tblock.dimension = 3;
 	return Tblock;
 }
 
 TetrisBlock IBlock() {
 	TetrisBlock Iblock;
-	Iblock.blockID = 1;
-	Iblock.cells[0] = { BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2),BlockPosition(1,3) };
+	Iblock.blockID = 7;
+	Iblock.matrix = {0, 0, 1, 0,
+					 0, 0, 1, 0,
+					 0, 0, 1, 0,
+					 0, 0, 1, 0};
+	Iblock.dimension = 4;
 	return Iblock;
 }
 
@@ -78,11 +100,39 @@ void TetrisBlock::Movement(int rows, int columns){
 }
 
 vector<BlockPosition> TetrisBlock::GetBlockPosition() {
-	vector<BlockPosition> tiles = cells[0];
 	vector<BlockPosition> movedTiles;
-	for (BlockPosition pos : tiles) {
-		BlockPosition newPos = BlockPosition(pos.row + rowOff, pos.col + colOff);
-		movedTiles.push_back(newPos);
+	for (int row = 0; row < dimension; ++row) {
+		for (int col = 0; col < dimension; ++col) {
+			
+			bool cell = false;
+			switch (currentRotation)
+				{
+				case TetrisBlock::Rotation::UP:
+					cell = matrix[row * dimension + col];
+					break;
+				case TetrisBlock::Rotation::RIGHT:
+					cell = matrix[dimension * (dimension - 1) - dimension * col + row];
+					break;
+				case TetrisBlock::Rotation::DOWN:
+					cell = matrix[(dimension * dimension - 1) - dimension * row - col];
+					break;
+				case TetrisBlock::Rotation::LEFT:
+					cell = matrix[dimension - 1 + dimension * col - row];
+					break;
+				default:
+					break;
+				}
+
+			if (cell) {
+				movedTiles.push_back(BlockPosition(row + rowOff, col + colOff));
+			}
+		}
 	}
 	return movedTiles;
+}
+
+void TetrisBlock::RotateClockwise() {
+
+	currentRotation = Rotation((int(currentRotation) + 1) % 4);
+
 }
